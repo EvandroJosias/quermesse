21cc3fdf59ecedf908ea85d02246b38a9f680e0b
EvandroJosias <evandro.josias@yahoo.com.br>
2025-05-05 14:28:35 -0300

    Initial Commit

---
+++ src/test/java/com/ejsjose/quermesse/QuermesseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejsjose.quermesse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuermesseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
